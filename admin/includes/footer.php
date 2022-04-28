4ae51be3a43aa58b13d439d21fa16da50774bead
sbmSifat <iamsbm.sifat@gmail.com>
2022-04-28 10:02:08 +0600

    sifat

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+     <strong>Copyright &copy; 2022 <a href="http://localhost/ecommerce/index.php">Sifat's IT Solutions</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2022 <a href="http://localhost/ecommerce/index.php"> Sifat's IT Solutions</a></strong>
+    </div>
+</footer>
